fa8de00779352d644439a5dc54c3cf4981de2856
HoyaBoya <christopher.sun@gmail.com>
2014-07-14 17:06:50 -0400

    more changes for heroku

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -6,9 +6,6 @@
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
 Spree.config do |config|
-  # Example:
-  # Uncomment to stop tracking inventory levels in the application
-  # config.track_inventory_levels = false
 end
 
 Spree.user_class = "Spree::User"
