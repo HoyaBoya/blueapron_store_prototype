da1a1d95b9aa9ac6cf5ab978125f0075c8d36bda
HoyaBoya <christopher.sun@gmail.com>
2014-07-14 17:46:24 -0400

    spree s3 stuff for latest

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -6,16 +6,6 @@
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
 Spree.config do |config|
-  if Rails.env.production?
-    config.use_s3 = true
-    config.s3_bucket = ENV['S3_BUCKET']
-    config.s3_access_key = ENV['S3_ACCESS_KEY']
-    config.s3_secret = ENV['S3_SECRET']
-  end
 end
 
 Spree.user_class = "Spree::User"
-
-Paperclip.interpolates(:s3_eu_url) do |attachment, style|
-"#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
-end
